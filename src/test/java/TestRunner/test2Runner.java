2c1ad6aefed80586d070050560d0bf02d3e1bd5d
Rahul Acc <Rahul Acc>
2019-05-12 17:15:43 +0100

    Adding parallel test

---
+++ src/test/java/TestRunner/test2Runner.java
@@ -0,0 +1,21 @@
+package TestRunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+//"pretty","html:target/cucumber-reports"}
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty","html:target/cucumber-reports"},
+glue= {"StepDefinition"},
+features= {"src/test/java/features"}
+
+)
+
+public class test2Runner {
+
+
+
+
+}
